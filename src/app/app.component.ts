31f227e71bd1c1f1529601923590d7d5b062b13f
Datn7 <gdatunashvili@weinor.de>
2025-03-10 02:27:07 +0400

    add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { FooterComponent } from './footer/footer.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LandingPageComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    LandingPageComponent,
+    FooterComponent,
+    NavigationComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
